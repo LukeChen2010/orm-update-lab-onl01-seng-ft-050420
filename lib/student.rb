05b8a837166c5b4bf620dcb9bd651a8b850cbb89
Luke Chen <luke.chen.2010@gmail.com>
2020-05-25 19:25:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,4 +53,12 @@ class Student
     student.save
     return student
   end
+  
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    return new_student
+  end
 end

7349a29dd75864e1ac01f0b90c3f6509b3030bf1
Luke Chen <luke.chen.2010@gmail.com>
2020-05-25 19:18:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,7 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_reader :name, :grade, :id
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]

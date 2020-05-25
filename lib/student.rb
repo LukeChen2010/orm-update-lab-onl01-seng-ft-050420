12ef161c896066693013880e04bf1b671385a2ad
Luke Chen <luke.chen.2010@gmail.com>
2020-05-25 19:19:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,5 +23,13 @@ class Student
   end
   
   def self.drop_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
   end
 end
